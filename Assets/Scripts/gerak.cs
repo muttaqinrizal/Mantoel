577586017d4d9f4a35aa16f5ec409ec4da26d070
muttaqinrizal <rizalm57@gmail.com>
2018-12-27 23:36:06 +0700

    pertama

---
+++ Assets/Scripts/gerak.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gerak : MonoBehaviour {
+    //public untuk global variabel
+    public float kecepatan=10;
+    float cepat = 20;
+    Vector2 ball;
+    Rigidbody2D lompat;
+	// Use this for initialization
+	void Start () {
+        //harus diinialisasi
+        lompat = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get key sebagai inputan tombol, keycode berdasarkan code tombol
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Translate untuk pergerakan object, vector2 merupakan vector x dan y, vector3 xyz
+            transform.Translate(Vector2.right * kecepatan * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * kecepatan * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            lompat.AddForce(new Vector2(0, cepat));
+        }
+
+
+	}
+}
